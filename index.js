0734141571cef3b3e3a23f2fc393d421cfdcf56c
Tom Bangle <tombangle@gmail.com>
2024-06-02 18:42:56 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  static titleize(str) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+    const capitalizedWords = words.map((word, index) => {
+      if (index === 0 || !smallWords.includes(word.toLowerCase())) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+      return word;
+    });
+    return capitalizedWords.join(' ');
+  }
 }
